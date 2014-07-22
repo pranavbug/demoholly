aaa20688d39f689ce10a1c6d219c688237dc9db7
pranavbug <becoolwithmepranav@gmail.com>
2014-07-22 22:35:52 +0530

    initial

---
+++ Holly/src/com/example/holly/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.holly;
+
+import com.example.holly.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+}
